a046930d8e53f2b2ffb1600acf27f3c1fb96e17b
seb_ritt <sebjpmine@gmail.com>
2024-11-13 10:09:38 +0100

    Modified the soble function to do a average filter (code based on chat gpt)

--- src/sobel.c
+++ src/sobel.c
@@ -17,6 +17,23 @@
 void sobel(int width, int height, unsigned char *input, unsigned char *output) {
   int i, j;
 
+    // Apply the averaging filter SOURCE CHAT-GPT FLEMME DE CODER CELA
+    for (j = 1; j < height - 1; j++) {
+        for (i = 1; i < width - 1; i++) {
+            // Calculate the average of the 3x3 neighborhood
+            int sum = input[(j - 1) * width + (i - 1)] + input[(j - 1) * width + i] + input[(j - 1) * width + (i + 1)]
+                      + input[j * width + (i - 1)] + input[j * width + i] + input[j * width + (i + 1)]
+                      + input[(j + 1) * width + (i - 1)] + input[(j + 1) * width + i] + input[(j + 1) * width + (i + 1)];
+            output[j * width + i] = sum / 9;
+        }
+    }
+
+    // Fill the left and right sides (optional, here we zero them)
+    for (j = 0; j < height - 2; j++) {
+        output[j * width] = 0;
+        output[(j + 1) * width - 1] = 0;
+    }
+    /*
   // Apply the filter
   for (j = 1; j < height - 1; j++) {
     for (i = 1; i < width - 1; i++) {
@@ -36,5 +53,6 @@ void sobel(int width, int height, unsigned char *input, unsigned char *output) {
   for (j = 0; j < height - 2; j++) {
     output[j * width] = 0;
     output[(j + 1) * width - 1] = 0;
-  }
+  }*/
+
 }
